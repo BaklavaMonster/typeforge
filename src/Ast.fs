44c20ef27d2bf069b5eba62bbdbab0f5a6574f32
Baklava Monster <dev@baklava.monster>
2025-07-10 16:55:25 +0200

    first working version of TypeForge
    -- implements typeEval, kindCheck and toJsonSchema
    -- provides tests for typeEval and kindCheck
    -- devcontainer ready!

--- src/Ast.fs
+++ src/Ast.fs
@@ -25,17 +25,18 @@ and TypeExpr =
     | Map of TypeExpr * TypeExpr
     | KeyOf of TypeExpr
     | Sum of List<TypeExpr>
-    | Flatten of FlattenArgs
+    | Flatten of TypeExpr * TypeExpr
+    // | Flatten of FlattenArgs // remove this as it makes no sense
     | Exclude of TypeExpr * TypeExpr
     | Rotate of TypeExpr
 
-and FlattenArgs =
-    { Left: TypeBinding
-      Right: TypeBinding }
+// and FlattenArgs =
+//     { Left: TypeBinding
+//       Right: TypeBinding }
 
-and TypeBinding =
-    { Identifier: TypeIdentifier
-      Type: TypeExpr }
+// and TypeBinding =
+//     { Identifier: TypeIdentifier
+//       Type: TypeExpr }
 
 and TypeValue =
     | Primitive of PrimitiveType
@@ -58,3 +59,22 @@ and PrimitiveType =
     | Decimal
     | Bool
     | String
+    | StringLiteral of string
+
+
+
+type Environment<'a> = {
+    map : Map<string,'a>
+}
+with 
+    static member get (env:Environment<'a>) k = 
+        match env.map.TryFind k with 
+        | Some v -> Ok v
+        | None -> sprintf "Could not find key %s in environment" k |> Error
+
+    static member put (env:Environment<'a>) kvp =
+        match env.map.ContainsKey (fst kvp) with
+        | false -> 
+            let newMap = env.map.Add kvp 
+            {env with map = newMap} |> Ok 
+        | true -> sprintf "Key %s is already defined in environment" (fst kvp) |> Error
--- src/Workbench.fs
+++
@@ -1,9 +0,0 @@
-module TypeForge.Workbench
-
-open TypeForge.Ast
-let t = 0
-
-let typeEval (e: TypeExpr) : TypeValue =
-    match e with
-    | TypeExpr.Primitive p -> Primitive p
-    | _ -> failwith "case not covered, yet..."
